895ad7b4679c7272a519775e779ad19d9eac8b53
JelloMangune <mangune.jello@auf.edu.ph>
2022-12-20 15:23:14 +0800

    first commit

---
+++ insert.php
@@ -0,0 +1,33 @@
+<?php
+
+include('vendor/autoload.php');
+use GuzzleHttp\Client;
+
+define('MONGODB_API_BASE_URL', 'https://data.mongodb-api.com/app/data-miegi/endpoint/data/v1/');
+define('API_KEY', 'nAWyd6Dl4TkWVGC8f9OFaSIZ3ygk0U3JXXF8WLmbNmSakcVNXiRqbFKqbLTMx6Dh');
+
+$client = new Client([
+    'base_uri' => MONGODB_API_BASE_URL,
+    'headers' => [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'api-key' => API_KEY
+    ]
+]);
+
+$registration_endpoint = 'action/insertOne';
+
+$data = $client->post($registration_endpoint, [
+        'json' => [
+        'dataSource' => 'Cluster0',
+        'database' => 'quiz',
+        'collection' =>'sample',
+        'document' => ['name' => 'Aaron'],
+        ]
+     
+]);
+
+var_dump($data);
+
+
+?>
